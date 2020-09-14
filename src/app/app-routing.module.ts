efedc51d65ea5e886977ec27a44b0f97bc129edb
Facco99 <facchin810@gmail.com>
2020-09-14 11:38:27 +0200

    sistemato css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) }, 
   { path: 'login', loadChildren: () => import('./features/login/login.module').then(m => m.LoginModule) },
-  { path: 'register', loadChildren: () => import('./features/register/register.module').then(m => m.RegisterModule) }
+  { path: 'register', loadChildren: () => import('./features/register/register.module').then(m => m.RegisterModule) },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
 
 @NgModule({
