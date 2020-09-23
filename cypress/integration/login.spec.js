155d6d8a5c6bfb7666ca650662d36d3bc8c55468
Facco99 <facchin810@gmail.com>
2020-09-23 10:16:47 +0200

    sistemato test

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,17 +1,16 @@
 context('Login', () => {
     beforeEach(() => {
-        cy.visit('localhost:4200')
-        sessionStorage.removeItem('utente');
+        cy.visit('localhost:4200');
     })
     it('Login success', () => {
         cy.get('#email').type('alefacco').should('have.value', 'alefacco');
-        cy.get('#password').type('ciao1').should('have.value', 'ciao1');
+        cy.get('#pass').type('ciao1').should('have.value', 'ciao1');
         cy.get('#submit').click();
-        cy.get('.title').should('have.text', "Benvenuto su SposiStore");
+        cy.get('.title').should('have.text', "Welcome to SposiStore");
     })
     it('Login fail', () => {
         cy.get('#email').type('fail').should('have.value', 'fail');
-        cy.get('#password').type('fail').should('have.value', 'fail');
+        cy.get('#pass').type('fail').should('have.value', 'fail');
         cy.get('#submit').click();
         cy.get('#error').should('have.text', 'Mail o password errate')
     })
